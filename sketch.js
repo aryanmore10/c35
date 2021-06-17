4c7c16f98b6fac4daca039d380e45dc56c2d0bdb
aryanmore10 <76562102+aryanmore10@users.noreply.github.com>
2021-06-17 14:15:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var ball;
+ var database,position;
+  function setup(){ 
+      database=firebase.database(); 
+      createCanvas(500,500);
+       ball = createSprite(250,250,10,10);
+        ball.shapeColor = "red";
+         var locOfchild=database.ref("Ball/Positions");
+          locOfchild.on("value", readOp,showerror);
+         } 
+         function draw(){
+              background("white"); 
+              if(keyDown(LEFT_ARROW)){
+                   writePosition(-1,0); 
+                } 
+                else if(keyDown(RIGHT_ARROW)){
+                     writePosition(1,0);
+                     } 
+                     else if(keyDown(UP_ARROW)){
+                          writePosition(0,-1);
+                         }
+                          else if(keyDown(DOWN_ARROW)){
+                               writePosition(0,+1);
+                             } 
+                             drawSprites();
+                             } 
+                             function writePosition(x,y){
+ database.ref("Ball/Positions").set({ x:ball.x = ball.x + x, y:ball.y = ball.y + y })
+ }
+  function readOp(data) {
+       position=data.val();
+        ball.x=position.x;
+        ball.y=position.y;
+     }
+      function showerror() {
+           console.log("error")
+         }
